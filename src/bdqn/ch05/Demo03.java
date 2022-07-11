678041f83c7ecc27c112fb1a7c7c3cddeed8f17b
hyj980606 <1841406870@qq.com>
2022-07-11 15:41:58 +0800

    2022-07-11

---
+++ src/bdqn/ch05/Demo03.java
@@ -0,0 +1,24 @@
+package bdqn.ch05;
+
+/**
+ * @ClassName Demo03
+ * @Description 20以内所有能被3整除的数之和
+ * @Author hyj98
+ * @Date 2022-07-11 15:26
+ * @Version 1.0
+ */
+
+public class Demo03 {
+    public static void main(String[] args) {
+        int i = 1;
+        int sum = 0;
+        while (i <= 20){
+            if (i % 3 == 0){
+                System.out.println(i);
+                sum = i + sum;
+            }
+            i++;
+        }
+        System.out.println("20以内所有能被3整除的数之和：" + sum);
+    }
+}
