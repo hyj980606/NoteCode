5623d5b7554c6e922affe50367bf06c7ad7672e7
hyj980606 <1841406870@qq.com>
2022-07-11 14:46:55 +0800

    2022-07-11

--- src/bdqn/ch04/Demo01.java
+++ src/bdqn/ch04/Demo01.java
@@ -5,8 +5,28 @@ import java.util.Scanner;
 /**
  * @ClassName Demo01
  * @Description                 switch选择结构
- *              break:结束
- *              default:
+ *              1、语法：
+ *                  switch(表达式) {
+ *                      case 常量1:
+ *                          语句;
+ *                          break;
+ *                      case 常量2:
+ *  *                       语句;
+ *  *                       break;
+ *                      case 常量3:
+ *  *                       语句;
+ *  *                       break;
+ *                      default:
+ *                          语句;
+ *                          break;
+ *                  }
+ *              2、break:结束,跳出循环
+ *              3、default:
+ *              4、比较switch和if选择结构
+ *                  相同点：
+ *                      都是用来处理
+ *                  不同点：
+ *
  * @Author hyj
  * @Date 2022-07-11 13:42
  * @Version 1.0
@@ -15,8 +35,7 @@ import java.util.Scanner;
 public class Demo01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("请输入月份：");
-        int month = input.nextInt();
+        System.out.print("请输入月份：");
 
 
         /*if (month == 7) {
@@ -31,21 +50,26 @@ public class Demo01 {
             System.out.println("网站推荐");
         }*/
 
-        switch (month){
-            case 7:
-                System.out.println("去青岛");
-                break;
-            case 8:
-                System.out.println("去西藏");
-                break;
-            case 9:
-                System.out.println("去新疆");
-                break;
-            case 10:
-                System.out.println("去九寨沟");
-                break;
-            default:
-                System.out.println("网站推荐");
+        if (input.hasNextInt()){
+            int month = input.nextInt();
+            switch (month){
+                case 7:
+                    System.out.println("去青岛");
+                    break;
+                case 8:
+                    System.out.println("去西藏");
+                    break;
+                case 9:
+                    System.out.println("去新疆");
+                    break;
+                case 10:
+                    System.out.println("去九寨沟");
+                    break;
+                default:
+                    System.out.println("网站推荐");
+            }
+        }else {
+            System.out.println("你输入的不是数字！");
         }
     }
 }
