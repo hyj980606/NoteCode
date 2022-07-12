460efedae76d006861cb912ec27f204960ab4328
hyj980606 <1841406870@qq.com>
2022-07-12 15:17:12 +0800

    2022-07-12

---
+++ src/bdqn/ch06/Demo01.java
@@ -0,0 +1,18 @@
+package bdqn.ch06;
+
+/**
+ * @ClassName Demo01
+ * @Description                for循环
+ *             for(参数初始化;条件;)
+ * @Author hyj98
+ * @Date 2022-07-12 15:01
+ * @Version 1.0
+ */
+
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            System.out.println(i);
+        }
+    }
+}
