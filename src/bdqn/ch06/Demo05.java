bc0f137bb370099ab8289db7e3eb6aae601e9193
hyj980606 <1841406870@qq.com>
2022-07-14 14:58:09 +0800

    2022-07-14

---
+++ src/bdqn/ch06/Demo05.java
@@ -0,0 +1,21 @@
+package bdqn.ch06;
+
+/**
+ * @ClassName Demo05
+ * @Description TODO
+ * @Author hyj98
+ * @Date 2022-07-14 14:51
+ * @Version 1.0
+ */
+
+public class Demo05 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            if (i==3){
+                System.out.println("口渴了跑不动退出");
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
