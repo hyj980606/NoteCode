84bca0e0725b754ca91a67ec5acc7de30e29b5f0
hyj980606 <1841406870@qq.com>
2022-07-15 16:13:37 +0800

    2022-07-15

---
+++ src/bdqn/ch07/Demo01.java
@@ -0,0 +1,39 @@
+package bdqn.ch07;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName Demo01
+ * @Description
+ * @Author hyj
+ * @Date 2022-07-15 15:56
+ * @Version 1.0
+ */
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //声明
+        String[] names = new String[6];
+
+        //循环输入蛋糕名称
+        for (int i = 0; i< names.length;i++){
+            System.out.print("请输入第"+(i+1)+"蛋糕名称:");
+            names[i] = input.next();
+        }
+        //先打印第四个蛋糕
+        System.out.println("打印蛋糕名称：");
+        for (int i = 0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+
+        /*names[0] = "草莓蛋糕";
+
+        String[] names1;
+        names1 = new String[6];
+        names1[0] = "冰淇淋蛋糕";
+
+        String[] names2 = {"a","b","c"};  */
+
+    }
+}
