c0c6f9f058d3e7fb1c38657267fba603ae85d0ce
hyj980606 <1841406870@qq.com>
2022-07-19 14:18:56 +0800

    九九乘法表

---
+++ src/bdqn/ch09/Demo03.java
@@ -0,0 +1,22 @@
+package bdqn.ch09;
+
+import javax.swing.*;
+
+/**
+ * @ClassName Demo03
+ * @Description 九九乘法表
+ * @Author hyj
+ * @Date 2022-07-19 14:09
+ * @Version 1.0
+ */
+
+public class Demo03 {
+    public static void main(String[] args) {
+        for (int i=1; i<=9;i++){
+            for (int j=1; j<=i; j++){
+                System.out.println(j+"*"+i+"="+(i*j)+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
