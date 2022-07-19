3e3a41872707e7d2e2aefb987c425792d017f4cb
hyj980606 <1841406870@qq.com>
2022-07-19 14:26:56 +0800

    九九乘法表

--- src/bdqn/ch09/Demo03.java
+++ src/bdqn/ch09/Demo03.java
@@ -14,7 +14,7 @@ public class Demo03 {
     public static void main(String[] args) {
         for (int i=1; i<=9;i++){
             for (int j=1; j<=i; j++){
-                System.out.println(j+"*"+i+"="+(i*j)+"\t");
+                System.out.print(j+"*"+i+"="+(i*j)+"\t");
             }
             System.out.println("");
         }
