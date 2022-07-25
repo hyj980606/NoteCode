ba5947703b74181de39ec408abb95628ef1dedf6
hyj980606 <1841406870@qq.com>
2022-07-25 16:11:06 +0800

    有参方法

---
+++ src/bdqn/ch14/Juicer.java
@@ -0,0 +1,17 @@
+package bdqn.ch14;
+
+/**
+ * @ClassName Juicer
+ * @Description 有参方法
+ * @Author hyj
+ * @Date 2022-07-25 16:05
+ * @Version 1.0
+ */
+
+public class Juicer {
+
+    public String  fruit(String fruit){
+        String fruits = fruit+"汁";
+        return fruits;
+    }
+}
