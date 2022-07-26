fb12a05b7bf32c9b480d4b7dfc78498c3c609106
hyj980606 <1841406870@qq.com>
2022-07-26 17:13:39 +0800

    引用数据类型

---
+++ src/bdqn/ch14/gifBiz/GifBiz.java
@@ -0,0 +1,20 @@
+package bdqn.ch14.gifBiz;
+
+/**
+ * @ClassName GifBiz
+ * @Description
+ * @Author hyj
+ * @Date 2022-07-26 16:19
+ * @Version 1.0
+ */
+
+public class GifBiz {
+
+    String name;//纪念品名称
+    double price;//价格
+    String place;//产地
+    public void shows(){
+        System.out.println("产品名称"+name+"，商品价格"+price+"，商品产地"+place);
+
+    }
+}
