8c5002e5d673b7df482a78ac72acbd28f4e88545
hyj980606 <1841406870@qq.com>
2022-07-26 15:38:58 +0800

    数组传参

---
+++ src/bdqn/ch14/calPrice/TestCalPrice.java
@@ -0,0 +1,30 @@
+package bdqn.ch14.calPrice;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName TestCalPrice
+ * @Description TODO
+ * @Author hyj98
+ * @Date 2022-07-26 15:21
+ * @Version 1.0
+ */
+
+public class TestCalPrice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入商品价格：");
+
+        CalPrice calPrice = new CalPrice();
+
+        double[]moneys=new double[5];
+        for (int i=0;i< moneys.length;i++){
+            moneys[i] = input.nextDouble();
+        }
+        double avg = calPrice.getAvg(moneys);
+        System.out.println(avg);
+
+        double sum = calPrice.getSum(moneys);
+        System.out.println(sum);
+    }
+}
